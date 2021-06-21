8c006d9a80f808d589081cbac7ada40ec6e84715
syafi <syafi@ASUS-HEROIII>
2021-06-21 12:06:41 +0800

    Initial Commit

---
+++ Exersice1/src/itemProduct.java
@@ -0,0 +1,32 @@
+public class itemProduct {
+	
+	private int itemProduct;
+	private String name;
+	private float price;
+	
+	public int getItemProduct() {
+		return itemProduct;
+	}
+	
+	public void setItemProduct(int itemProduct) {
+		this.itemProduct = itemProduct;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public float getPrice() {
+		return price;
+	}
+	
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	
+}
